e7a7fa6d7ff44788e4e79806695f7c53aa62f6d3
Bilgehan Kışo <73483481+bilgehankiso@users.noreply.github.com>
2026-04-17 21:25:20 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Basit Faiz Hesaplama Scripti
 
 echo "Ana para miktarını girin: "
 read principal
@@ -8,7 +7,6 @@ read rate
 echo "Zaman dilimini girin (yıl): "
 read time
 
-# Basit faiz formülü: faiz = ana_para * oran * zaman / 100
 interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
 total=$(echo "scale=2; $principal + $interest" | bc)
 
